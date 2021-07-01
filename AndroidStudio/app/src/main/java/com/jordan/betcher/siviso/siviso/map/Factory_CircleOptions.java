934f8f62f018ca9fc52edf4051e44cf5f7772c89
Plum <pplluumm@users.noreply.github.com>
2021-06-30 20:13:16 -0700

    initial Factory_CircleOptions

--- AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/map/Factory_CircleOptions.java
+++ AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/map/Factory_CircleOptions.java
@@ -1,12 +1,22 @@
 package com.jordan.betcher.siviso.siviso.map;
 
+import android.graphics.Color;
+
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PatternItem;
 
 class Factory_CircleOptions
 {
 	public CircleOptions create(LatLng latLng)
 	{
-		return null;
+		CircleOptions circleOptions = new CircleOptions();
+		circleOptions.center(latLng);
+		circleOptions.radius(1000);
+		circleOptions.visible(true);
+		circleOptions.strokeWidth(10);
+		circleOptions.fillColor(Color.GRAY);
+		circleOptions.strokeColor(Color.GREEN);
+		return circleOptions;
 	}
 }
