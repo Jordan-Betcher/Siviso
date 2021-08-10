19309d76f1198648cd375fe5a630959fe3b7bd40
Plum <pplluumm@users.noreply.github.com>
2021-08-10 11:50:56 -0700

    ArrayAdapter_CharSequence name

--- AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/list/viewholderdefault/Test$ViewHolder_Default.java
+++ AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/list/viewholderdefault/Test$ViewHolder_Default.java
@@ -8,34 +8,62 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
+
 import com.jordan.betcher.siviso.siviso.list.ArrayAdapter_CharSequence;
 
 import org.junit.Test;
 
 public class Test$ViewHolder_Default
 {
+	
 	@Test
 	public void init_spinnerArrayAdapter_SetSpinnerAdapterToArrayAdapter()
 	{
-		Spinner spinner = mock(Spinner.class);
-		ArrayAdapter_CharSequence sivisoSpinnerAdapter = mock(ArrayAdapter_CharSequence.class);
 		View view = mock(View.class);
+		TextView textView = mock(TextView.class);
+		String name = "";
+		Spinner spinner = mock(Spinner.class);
+		ArrayAdapter_CharSequence adapter = mock(ArrayAdapter_CharSequence.class);
+		OnItemClickListener_SetDefaultSiviso onItemClickListener = mock(OnItemClickListener_SetDefaultSiviso.class);
+		CardView cardView = mock(CardView.class);
+		OnClickListener_GoToCurrentLocation onClick = mock(OnClickListener_GoToCurrentLocation.class);
 		
-		ViewHolder_Default viewHolder = new ViewHolder_Default(view, mock(TextView.class), "", spinner, sivisoSpinnerAdapter, null, null, null);
-		viewHolder.init(null, 0);
+		new ViewHolder_Default(view, textView, name, spinner, adapter, onItemClickListener, cardView, onClick);
 		
-		verify(spinner, times(1)).setAdapter(sivisoSpinnerAdapter);
+		verify(spinner, times(1)).setAdapter(adapter);
 	}
 	
 	@Test
-	public void init_textViewNameDefault_SetTextViewTextToNameDefault()
+	public void _textViewName_SetTextViewTextToName()
 	{
-		String name = "Default";
-		TextView textView = mock(TextView.class);
 		View view = mock(View.class);
+		TextView textView = mock(TextView.class);
+		String name = "";
+		Spinner spinner = mock(Spinner.class);
+		ArrayAdapter_CharSequence adapter = mock(ArrayAdapter_CharSequence.class);
+		OnItemClickListener_SetDefaultSiviso onItemClickListener = mock(OnItemClickListener_SetDefaultSiviso.class);
+		CardView cardView = mock(CardView.class);
+		OnClickListener_GoToCurrentLocation onClick = mock(OnClickListener_GoToCurrentLocation.class);
 		
-		ViewHolder_Default viewHolder = new ViewHolder_Default(view, textView, name, mock(Spinner.class), mock(ArrayAdapter_CharSequence.class), null, null, null);
-		viewHolder.init(null, 0);
+		new ViewHolder_Default(view, textView, name, spinner, adapter, onItemClickListener, cardView, onClick);
+		
+		verify(textView, times(1)).setText(name);
+	}
+	
+	@Test
+	public void _textViewNameDefault_SetTextViewTextToNameDefault()
+	{
+		View view = mock(View.class);
+		TextView textView = mock(TextView.class);
+		String name = "Default";
+		Spinner spinner = mock(Spinner.class);
+		ArrayAdapter_CharSequence adapter = mock(ArrayAdapter_CharSequence.class);
+		OnItemClickListener_SetDefaultSiviso onItemClickListener = mock(OnItemClickListener_SetDefaultSiviso.class);
+		CardView cardView = mock(CardView.class);
+		OnClickListener_GoToCurrentLocation onClick = mock(OnClickListener_GoToCurrentLocation.class);
+		
+		new ViewHolder_Default(view, textView, name, spinner, adapter, onItemClickListener, cardView, onClick);
 		
 		verify(textView, times(1)).setText(name);
 	}
