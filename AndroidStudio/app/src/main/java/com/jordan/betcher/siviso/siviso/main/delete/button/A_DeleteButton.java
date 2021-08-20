5f4e4eb7e8e861a2c9fbdb48301be8cc734ab09a
Plum <pplluumm@users.noreply.github.com>
2021-08-20 10:09:25 -0700

    r removed Factory_DeleteButton into A_DeleteButton

--- AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/main/delete/button/A_DeleteButton.java
+++ AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/main/delete/button/A_DeleteButton.java
@@ -1,17 +1,33 @@
 package com.jordan.betcher.siviso.siviso.main.delete.button;
 
+import android.widget.Button;
+
+import com.jordan.betcher.siviso.siviso.R;
 import com.jordan.betcher.siviso.siviso.database.Database;
 import com.jordan.betcher.siviso.siviso.list.SivisoList;
 import com.jordan.betcher.siviso.siviso.main.A_Activity_Main;
 
 public class A_DeleteButton
 {
+	private A_Activity_Main activity;
+	private Database database;
+	private SivisoList sivisoList;
+	
 	public A_DeleteButton(A_Activity_Main activity, Database database, SivisoList sivisoList)
 	{
-		Factory_DeleteButton deleteButtonFactory = new Factory_DeleteButton(activity, database, sivisoList);
-		DeleteButton deleteButton = deleteButtonFactory.deleteButton();
+		this.activity = activity;
+		this.database = database;
+		this.sivisoList = sivisoList;
 		
+		DeleteButton deleteButton = deleteButton();
 		OnSelect_EnableButton onSelect = new OnSelect_EnableButton(deleteButton);
 		sivisoList.addOnSelect(onSelect);
+	}
+	
+	private DeleteButton deleteButton()
+	{
+		Button button = activity.findViewById(R.id.buttonDelete);
+		OnClickListener_DeleteSelected deleteSelected = new OnClickListener_DeleteSelected(database, sivisoList);
+		return new DeleteButton(button, deleteSelected);
 	}
 }
