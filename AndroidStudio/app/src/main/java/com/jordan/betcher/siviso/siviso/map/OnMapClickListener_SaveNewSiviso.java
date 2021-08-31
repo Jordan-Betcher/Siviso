1a212ed6c734cdb1ed3cc8d1266941fa17a4ae33
Plum <pplluumm@users.noreply.github.com>
2021-08-31 12:07:36 -0700

    r

--- AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/map/OnMapClickListener_SaveNewSiviso.java
+++ AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/map/OnMapClickListener_SaveNewSiviso.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.jordan.betcher.siviso.siviso.database.Database;
+import com.jordan.betcher.siviso.siviso.database.Siviso;
 
 class OnMapClickListener_SaveNewSiviso
 implements GoogleMap.OnMapClickListener
@@ -21,6 +22,7 @@ implements GoogleMap.OnMapClickListener
 	@Override
 	public void onMapClick(@NonNull LatLng latLng)
 	{
-		database.save(factorySiviso.create(latLng));
+		Siviso siviso = factorySiviso.create(latLng);
+		database.save(siviso);
 	}
 }
