a77b3384845b361403ad5633d52f4b2b0f911351
Plum <pplluumm@users.noreply.github.com>
2021-09-02 13:07:01 -0700

    t Test$IndexOfSiviso.from_01Siviso01Siviso00_0 and from_22Siviso01Siviso00_negative1

--- AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/map/IndexOfSiviso.java
+++ AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/map/IndexOfSiviso.java
@@ -2,17 +2,26 @@ package com.jordan.betcher.siviso.siviso.map;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.jordan.betcher.siviso.siviso.database.Database;
+import com.jordan.betcher.siviso.siviso.database.Siviso;
 
 class IndexOfSiviso
 {
+	Siviso[] sivisos;
+	
 	public IndexOfSiviso(Database database)
 	{
 		//TODO IndexOfSiviso IndexOfSiviso
+		this.sivisos = database.sivisos();
 	}
 	
 	public int from(LatLng latLng)
 	{
-		return 1; //TODO IndexOfSiviso from
+		for(int i = 0; i < sivisos.length; i++)
+		{
+			Siviso siviso = sivisos[i];
+			if(siviso.latLng().equals(latLng)) return i;
+		}
+		return -1; //TODO IndexOfSiviso from
 	}
 	//TODO
 }
--- AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/map/Test$IndexOfSiviso.java
+++ AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/map/Test$IndexOfSiviso.java
@@ -12,6 +12,45 @@ import org.junit.Test;
 
 public class Test$IndexOfSiviso
 {
+	
+	@Test
+	public void from_22Siviso01Siviso00_negative1()
+	{
+		int index = -1;
+		LatLng latLng = new LatLng(2, 2);
+		Database database = mock(Database.class);
+		Siviso siviso1 = mock(Siviso.class);
+		Siviso siviso2 = mock(Siviso.class);
+		Siviso[] sivisos = new Siviso[]{siviso1, siviso2};
+		when(siviso1.latLng()).thenReturn(new LatLng(0, 1));
+		when(siviso2.latLng()).thenReturn(new LatLng(0, 0));
+		when(database.sivisos()).thenReturn(sivisos);
+		
+		IndexOfSiviso indexOfSiviso = new IndexOfSiviso(database);
+		
+		int actualIndex = indexOfSiviso.from(latLng);
+		assertEquals(index, actualIndex);
+	}
+	
+	@Test
+	public void from_01Siviso01Siviso00_0()
+	{
+		int index = 0;
+		LatLng latLng = new LatLng(0, 1);
+		Database database = mock(Database.class);
+		Siviso siviso1 = mock(Siviso.class);
+		Siviso siviso2 = mock(Siviso.class);
+		Siviso[] sivisos = new Siviso[]{siviso1, siviso2};
+		when(siviso1.latLng()).thenReturn(new LatLng(0, 1));
+		when(siviso2.latLng()).thenReturn(latLng);
+		when(database.sivisos()).thenReturn(sivisos);
+		
+		IndexOfSiviso indexOfSiviso = new IndexOfSiviso(database);
+		
+		int actualIndex = indexOfSiviso.from(latLng);
+		assertEquals(index, actualIndex);
+	}
+	
 	@Test
 	public void from_00Siviso01Siviso00_1()
 	{
