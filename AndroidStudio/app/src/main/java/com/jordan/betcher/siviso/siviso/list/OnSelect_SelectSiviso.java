6815d56b08d1c0415c5f9f3610f7bf3ea1562ebf
Plum <pplluumm@users.noreply.github.com>
2021-09-03 12:35:46 -0700

    r remove tests for OnSelect_SelectSiviso

--- AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/list/OnSelect_SelectSiviso.java
+++ AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/list/OnSelect_SelectSiviso.java
@@ -4,18 +4,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 class OnSelect_SelectSiviso implements OnSelect, OnBindView
 {
-	private LinearLayoutManager linearLayoutManager;
 	
 	public OnSelect_SelectSiviso(LinearLayoutManager linearLayoutManager)
 	{
-		this.linearLayoutManager = linearLayoutManager;
 	}
 	
 	@Override
 	public void onSelect(int indexOfSiviso)
 	{
 		//TODO have sivisoList onSelect highlight that index
-		linearLayoutManager.scrollToPosition(indexOfSiviso + 1);
 	}
 	
 	@Override
--- AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/list/Test$OnSelect_SelectSiviso.java
+++ AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/list/Test$OnSelect_SelectSiviso.java
@@ -1,47 +1,6 @@
 package com.jordan.betcher.siviso.siviso.list;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import org.junit.Test;
-
 public class Test$OnSelect_SelectSiviso
 {
-	@Test
-	public void onSelect_1_scrollTo2()
-	{
-		int indexOfSiviso = 1;
-		LinearLayoutManager linearLayoutManager = mock(LinearLayoutManager.class);
-		
-		OnSelect_SelectSiviso scrollToIndexOfSiviso = new OnSelect_SelectSiviso(linearLayoutManager);
-		scrollToIndexOfSiviso.onSelect(indexOfSiviso);
-		
-		verify(linearLayoutManager, times(1)).scrollToPosition(indexOfSiviso + 1);
-	}
-	
-	@Test
-	public void __0scrollTo()
-	{
-		LinearLayoutManager linearLayoutManager = mock(LinearLayoutManager.class);
-		
-		new OnSelect_SelectSiviso(linearLayoutManager);
-		
-		verify(linearLayoutManager, times(0)).scrollToPosition(anyInt());
-	}
-	
-	@Test
-	public void onSelect_0_scrollTo1()
-	{
-		int indexOfSiviso = 0;
-		LinearLayoutManager linearLayoutManager = mock(LinearLayoutManager.class);
-		
-		OnSelect_SelectSiviso scrollToIndexOfSiviso = new OnSelect_SelectSiviso(linearLayoutManager);
-		scrollToIndexOfSiviso.onSelect(indexOfSiviso);
-		
-		verify(linearLayoutManager, times(1)).scrollToPosition(indexOfSiviso + 1);
-	}
+
 }
