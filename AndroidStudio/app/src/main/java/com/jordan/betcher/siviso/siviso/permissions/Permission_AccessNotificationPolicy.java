80157aa09d1b3fbdaea40cf0c298323f866394e3
Plum <pplluumm@users.noreply.github.com>
2021-10-04 10:33:00 -0700

    t Permission_AccessNotificationPolicy __0startActivityForResult

--- AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/permissions/Permission_AccessNotificationPolicy.java
+++ AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/permissions/Permission_AccessNotificationPolicy.java
@@ -5,15 +5,19 @@ import android.app.Activity;
 public class Permission_AccessNotificationPolicy implements Permission
 {
 	
+	private final Activity activity;
+	private final Intent_PermissionSettingNotification permissionSettingNotification;
+	
 	public Permission_AccessNotificationPolicy(Activity activity, Intent_PermissionSettingNotification permissionSettingNotification)
 	{
-		activity.startActivityForResult(permissionSettingNotification, 1);
+		this.activity = activity;
+		this.permissionSettingNotification = permissionSettingNotification;
 	}
 	
 	@Override
 	public void request()
 	{
-		//TODO
+		activity.startActivityForResult(permissionSettingNotification, 1);
 	}
 	
 	@Override
--- AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/permissions/Test$Permission_AccessNotificationPolicy.java
+++ AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/permissions/Test$Permission_AccessNotificationPolicy.java
@@ -1,5 +1,7 @@
 package com.jordan.betcher.siviso.siviso.permissions;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -10,6 +12,17 @@ import org.junit.Test;
 
 public class Test$Permission_AccessNotificationPolicy
 {
+	@Test
+	public void __0startActivityForResult()
+	{
+		Activity activity = mock(Activity.class);
+		Intent_PermissionSettingNotification permissionSettingNotification = mock(Intent_PermissionSettingNotification.class);
+		
+		new Permission_AccessNotificationPolicy(activity, permissionSettingNotification);
+		
+		verify(activity, times(0)).startActivityForResult(any(), anyInt());
+	}
+	
 	@Test
 	public void request__startActivityForResult()
 	{
