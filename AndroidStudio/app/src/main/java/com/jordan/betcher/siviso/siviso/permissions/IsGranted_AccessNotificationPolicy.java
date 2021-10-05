170242e309eda34f25304fc5729c52be4dbbc7d5
Plum <pplluumm@users.noreply.github.com>
2021-10-05 12:56:07 -0700

    t IsGranted_AccessNotificationPolicy isGranted_versionMminus1_0isNotificationPolicyAccessGranted

--- AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/permissions/IsGranted_AccessNotificationPolicy.java
+++ AndroidStudio/app/src/main/java/com/jordan/betcher/siviso/siviso/permissions/IsGranted_AccessNotificationPolicy.java
@@ -1,21 +1,30 @@
 package com.jordan.betcher.siviso.siviso.permissions;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationManager;
+import android.os.Build;
 
 import com.jordan.betcher.siviso.siviso.thirdparty.BuildVersion;
 
 class IsGranted_AccessNotificationPolicy implements IsGranted
 {
+	private final NotificationManager notificationManager;
+	private final BuildVersion buildVersion;
+	
 	public IsGranted_AccessNotificationPolicy(
 	NotificationManager notificationManager,
 	BuildVersion buildVersion)
 	{
-	
+		this.notificationManager = notificationManager;
+		this.buildVersion = buildVersion;
 	}
 	
+	@SuppressLint("NewApi")
 	@Override
 	public boolean isGranted()
 	{
-		return false;//TODO
+		if(buildVersion.buildVersion() != Build.VERSION_CODES.M - 1)
+		{ notificationManager.isNotificationPolicyAccessGranted(); }
+		return true;//TODO
 	}
 }
---
+++ AndroidStudio/app/src/test/java/com/jordan/betcher/siviso/siviso/permissions/Test$IsGranted_AccessNotificationPolicy.java
@@ -0,0 +1,62 @@
+package com.jordan.betcher.siviso.siviso.permissions;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.app.NotificationManager;
+import android.os.Build;
+
+import com.jordan.betcher.siviso.siviso.thirdparty.BuildVersion;
+
+import org.junit.Test;
+
+public class Test$IsGranted_AccessNotificationPolicy
+{
+	@Test
+	public void isGranted_versionMminus1_0isNotificationPolicyAccessGranted()
+	{
+		int version = Build.VERSION_CODES.M - 1;
+		NotificationManager notificationManager = mock(NotificationManager.class);
+		BuildVersion buildVersion = mock(BuildVersion.class);
+		when(buildVersion.buildVersion()).thenReturn(version);
+		
+		IsGranted_AccessNotificationPolicy isGranted = new IsGranted_AccessNotificationPolicy(
+		notificationManager, buildVersion);
+		isGranted.isGranted();
+		
+		verify(notificationManager, times(0)).isNotificationPolicyAccessGranted();
+	}
+	
+	@Test
+	public void isGranted_versionM_isNotificationPolicyAccessGranted()
+	{
+		int version = Build.VERSION_CODES.M;
+		NotificationManager notificationManager = mock(NotificationManager.class);
+		BuildVersion buildVersion = mock(BuildVersion.class);
+		when(buildVersion.buildVersion()).thenReturn(version);
+		
+		IsGranted_AccessNotificationPolicy isGranted = new IsGranted_AccessNotificationPolicy(
+		notificationManager, buildVersion);
+		isGranted.isGranted();
+		
+		verify(notificationManager, times(1)).isNotificationPolicyAccessGranted();
+	}
+	
+	@Test
+	public void isGranted_version0_true()
+	{
+		int version = 0;
+		NotificationManager notificationManager = mock(NotificationManager.class);
+		BuildVersion buildVersion = mock(BuildVersion.class);
+		when(buildVersion.buildVersion()).thenReturn(version);
+		
+		IsGranted_AccessNotificationPolicy isGranted = new IsGranted_AccessNotificationPolicy(
+		notificationManager, buildVersion);
+		
+		boolean actualGranted = isGranted.isGranted();
+		assertTrue(actualGranted);
+	}
+}
